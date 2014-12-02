0dc62c5791dee624eb03021a06e2b4541a199c1b
saifiahmada <saifiahmada@gmail.com>
2014-12-02 16:09:12 +0800

    Commit Pertama

---
+++ src/main/java/com/saifiahmada/maven/Test.java
@@ -0,0 +1,15 @@
+package com.saifiahmada.maven;
+
+public class Test {
+	
+	public void test () {
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		
+		
+		
+	}
+
+}
